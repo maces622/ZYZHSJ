dd0f075e949858bddff20adf504c735346890149
maces622 <1607531044@qq.com>
2023-12-25 17:19:24 +0800

    rc4 enc

--- test.py
+++ test.py
@@ -1,93 +1,94 @@
+import imp
+from logging import root
 import numpy as np
 from PIL import Image
+import os
+
+from PIL import Image
+import numpy as np
+
+# RC4 encryption algorithm
+class RC4:
+    def __init__(self, key=None):
+        self.state = list(range(256))  # Initialize state array
+        self.x = self.y = 0  # Set indices to 0
+
+        # Key-scheduling algorithm (KSA)
+        if key:
+            self.init_ksa(key)
+
+    def init_ksa(self, key):
+        j = 0
+        for i in range(256):
+            j = (j + self.state[i] + key[i % len(key)]) % 256
+            self.state[i], self.state[j] = self.state[j], self.state[i]
+
+    def encrypt(self, data):
+        return bytes([b ^ self.prga() for b in data])
+
+    def prga(self):  # Pseudo-random generation algorithm (PRGA)
+        self.x = (self.x + 1) % 256
+        self.y = (self.y + self.state[self.x]) % 256
+        self.state[self.x], self.state[self.y] = self.state[self.y], self.state[self.x]
+        return self.state[(self.state[self.x] + self.state[self.y]) % 256]
+
+# Function to encrypt an image using RC4
+def encrypt_image(image_path, key):
+    # Load the image and convert it to grayscale
+    img = Image.open(image_path).convert('L')
+    img_data = np.array(img)
+
+    # Flatten the image array and convert to bytes
+    flat_img_data = img_data.flatten()
+    byte_data = bytes(flat_img_data)
+
+    # Initialize RC4 with the provided key
+    rc4 = RC4(key)
+
+    # Encrypt the data
+    encrypted_data = rc4.encrypt(byte_data)
+
+    # Convert the encrypted data back to the original image dimensions
+    encrypted_img_data = np.frombuffer(encrypted_data, dtype=np.uint8).reshape(img_data.shape)
+
+    # Create an encrypted image from the data
+    encrypted_img = Image.fromarray(encrypted_img_data)
+    return encrypted_img
+
+# Path to the input image (replace with the path to your image)
+
+root_path=os.path.abspath(os.path.join(os.getcwd(), ".."))
+pic_path=os.path.join(root_path,"picture")
+original_image_path = pic_path+"\\7.jpg"
+key = b'secret_key'  # Update this key
+
+# Encrypt the image
+encrypted_image = encrypt_image(original_image_path, key)
+
+# Display the original and encrypted images
+original_image = Image.open(original_image_path).convert('L')
+original_image.show(title="Original Image")
+encrypted_image.show(title="Encrypted Image")
 
 
-def rc4(key, data):
-    S = list(range(256))
-    j = 0
-    for i in range(256):
-        j = (j + S[i] + key[i % len(key)]) % 256
-        S[i], S[j] = S[j], S[i]
-
-    i = j = 0
-    result = bytearray()
-    for byte in data:
-        i = (i + 1) % 256
-        j = (j + S[i]) % 256
-        S[i], S[j] = S[j], S[i]
-        result.append(byte ^ S[(S[i] + S[j]) % 256])
-
-    return result
+# # Example usage
+# root_path=os.path.abspath(os.path.join(os.getcwd(), ".."))
+# pic_path=os.path.join(root_path,"picture")
 
 
-def encrypt_image(original_image_path, encryption_key):
-    original_image = Image.open(original_image_path).convert('L')  # Convert to grayscale
-    pixels = np.array(original_image)
+# encryption_key = b'\x08\xfa\x06\xc6\xdd\xbd\xa4\x80'
+# original_image_path = pic_path+"\\7.jpg"
+# additional_data = '12345678'
 
-    encrypted_pixels = []
-    for row in pixels:
-        encrypted_row = []
-        for pixel in row:
-            encrypted_pixel = rc4(encryption_key, [int(pixel)])
-            encrypted_row.append(np.uint8(encrypted_pixel[0]))
-        encrypted_pixels.append(encrypted_row)
-
-    encrypted_image = Image.fromarray(np.array(encrypted_pixels, dtype=np.uint8))
-    encrypted_image.save("encrypted_image.png")
-    return "encrypted_image.png"
+# # Encrypt the original image
+# encrypted_image_path = encrypt_image(original_image_path, encryption_key)
 
 
-def embed_data(encrypted_image_path, data_hiding_key, additional_data):
-    encrypted_image = Image.open(encrypted_image_path)
-    pixels = np.array(encrypted_image)
+# """
+# # Embed data into the encrypted image
+# embedded_image_path = embed_data(encrypted_image_path, encryption_key, additional_data)
 
-    data_bits = [int(bit) for bit in '{:08b}'.format(int(additional_data))]
-    block_size = 8  # Assuming 8 bits of data in each block
-
-    for i in range(0, len(pixels), block_size):
-        block = pixels[i:i + block_size]
-        set_a = np.random.choice([0, 1], size=block.shape, p=[0.5, 0.5])
-
-        for j in range(block.shape[0]):
-            for k in range(block.shape[1]):
-                if data_bits[j] == 0:
-                    block[j][k] = np.bitwise_xor(block[j][k], set_a[j][k] & 0b111)  # Flip 3 LSB
-                else:
-                    block[j][k] = np.bitwise_xor(block[j][k], (~set_a[j][k] & 0b111))  # Flip 3 LSB
-
-    embedded_image = Image.fromarray(np.array(pixels, dtype=np.uint8))
-    embedded_image.save("embedded_image.png")
-    return "embedded_image.png"
-
-
-def decrypt_image(embedded_image_path, encryption_key):
-    embedded_image = Image.open(embedded_image_path)
-    pixels = np.array(embedded_image)
-
-    decrypted_pixels = []
-    for row in pixels:
-        decrypted_row = []
-        for pixel in row:
-            decrypted_pixel = rc4(encryption_key, [int(pixel)])
-            decrypted_row.append(np.uint8(decrypted_pixel[0]))
-        decrypted_pixels.append(decrypted_row)
-
-    decrypted_image = Image.fromarray(np.array(decrypted_pixels, dtype=np.uint8))
-    decrypted_image.save("decrypted_image.png")
-    return "decrypted_image.png"
-
-
-# Example usage
-encryption_key = b'\x08\xfa\x06\xc6\xdd\xbd\xa4\x80'
-original_image_path = '/Users/dominic/Downloads/grayscale_image.png'
-additional_data = '12345678'
-
-# Encrypt the original image
-encrypted_image_path = encrypt_image(original_image_path, encryption_key)
-
-# Embed data into the encrypted image
-embedded_image_path = embed_data(encrypted_image_path, encryption_key, additional_data)
-
-# Decrypt the embedded image
-decrypted_image_path = decrypt_image(embedded_image_path, encryption_key)
-print(f"Decrypted image saved as: {decrypted_image_path}")
+# # Decrypt the embedded image
+# decrypted_image_path = decrypt_image(embedded_image_path, encryption_key)
+# print(f"Decrypted image saved as: {decrypted_image_path}")
+# """
